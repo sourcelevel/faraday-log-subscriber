45cede92f928d8d4b337a049f1e4c38043861be5
Hannes Wüthrich <hw@5px.ch>
2016-07-13 00:52:13 +0200

    Failing tests because runtime is not reset between requests
    Test for ControllerRuntime more or less copied from
    https://github.com/rails/rails/blob/master/actionview/test/activerecord/controller_runtime_test.rb

--- test/controller_runtime_test.rb
+++ test/controller_runtime_test.rb
@@ -1,4 +1,90 @@
 require 'test_helper'
+require 'active_support/log_subscriber/test_helper'
 
-class ControllerRuntimeTest < ActiveSupport::TestCase
+
+class ControllerRuntimeTest < ActionController::TestCase
+
+  class LogSubscriberController < ActionController::Base
+    def zero
+      render inline: 'Zero HTTP runtime'
+    end
+
+    def show
+      Faraday::LogSubscriber.runtime += 100
+      render inline: 'Takes 100ms'
+    end
+
+    def redirect
+      Faraday::LogSubscriber.runtime += 100
+      redirect_to 'http://example.com'
+    end
+
+    def faraday_after_render
+      Faraday::LogSubscriber.runtime += 100
+      render inline: 'Hello world'
+      Faraday::LogSubscriber.runtime += 100
+    end
+  end
+
+
+  include ActiveSupport::LogSubscriber::TestHelper
+  tests LogSubscriberController
+
+
+  def setup
+    super
+
+    ActiveSupport::Deprecation.silence do
+      @routes = ActionDispatch::Routing::RouteSet.new
+      @routes.draw { get ':controller(/:action)' }
+    end
+
+    ActionController::LogSubscriber.attach_to :action_controller
+  end
+
+  def teardown
+    super
+
+    ActiveSupport::LogSubscriber.log_subscribers.clear
+    Faraday::LogSubscriber.reset_runtime
+  end
+
+  def set_logger(logger)
+    ActionController::Base.logger = logger
+  end
+
+  def test_runtime_reset_before_requests
+    Faraday::LogSubscriber.runtime += 12345
+    get :zero
+    wait
+
+    assert_equal 2, @logger.logged(:info).size
+    assert_match(/Faraday: 0.0ms\)/, @logger.logged(:info)[1])
+  end
+
+  def test_runtime_reset_between_requests
+    get :show
+    get :show
+    wait
+
+    assert_equal 4, @logger.logged(:info).size
+    assert_match(/Faraday: 100.0ms\)/, @logger.logged(:info)[1])
+    assert_match(/Faraday: 100.0ms\)/, @logger.logged(:info)[3])
+  end
+
+  def test_log_with_faraday_time_when_redirecting
+    get :redirect
+    wait
+
+    assert_equal 3, @logger.logged(:info).size
+    assert_match(/\(Faraday: [\d.]+ms\)/, @logger.logged(:info)[2])
+  end
+
+  def test_include_faraday_time_after_rendering
+    get :faraday_after_render
+    wait
+
+    assert_equal 2, @logger.logged(:info).size
+    assert_match(/Faraday: 200.0ms\)/, @logger.logged(:info)[1])
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'bundler/setup'
 require 'active_support'
 require 'active_support/log_subscriber'
 
+require 'action_controller'
+require 'action_controller/log_subscriber'
+
 require 'faraday'
 require 'faraday_middleware'
 
